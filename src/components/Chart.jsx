14ef6743833913266a4b02bcfb08ccc26a765316
Pablo Sellarés <pablosellares@live.com.ar>
2024-05-12 07:50:07 -0300

    Fix Chart data and styles

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -5,68 +5,67 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  Legend,
 } from "recharts";
 
 const data = [
   {
     name: "4",
-    newFollowers: 4000,
-    followersLast10days: 2400,
+    followersLast10days: 4000,
+    newFollowers: 3,
     amt: 2,
   },
   {
     name: "5",
-    newFollowers: 3000,
-    followersLast10days: 1398,
+    followersLast10days: 3000,
+    newFollowers: 2,
     amt: 2210,
   },
   {
     name: "6",
-    newFollowers: 2000,
-    followersLast10days: 9800,
+    followersLast10days: 2000,
+    newFollowers: 6,
     amt: 2290,
   },
   {
     name: "7",
-    newFollowers: 2780,
-    followersLast10days: 3908,
+    followersLast10days: 2780,
+    newFollowers: 8,
     amt: 2000,
   },
   {
     name: "8",
-    newFollowers: 1890,
-    followersLast10days: 4800,
+    followersLast10days: 1890,
+    newFollowers: 9,
     amt: 2181,
   },
   {
     name: "9",
-    newFollowers: 2390,
-    followersLast10days: 3800,
+    followersLast10days: 2390,
+    newFollowers: 10,
     amt: 2500,
   },
   {
     name: "10",
-    newFollowers: 3490,
-    followersLast10days: 4300,
+    followersLast10days: 3490,
+    newFollowers: 9,
     amt: 2100,
   },
   {
     name: "11",
-    newFollowers: 3490,
-    followersLast10days: 4300,
+    followersLast10days: 3490,
+    newFollowers: 10,
     amt: 2100,
   },
   {
     name: "12",
-    newFollowers: 3490,
-    followersLast10days: 4300,
+    followersLast10days: 3490,
+    newFollowers: 12,
     amt: 2100,
   },
   {
     name: "13",
-    newFollowers: 3490,
-    followersLast10days: 4300,
+    followersLast10days: 3490,
+    newFollowers: 12,
     amt: 2100,
   },
 ];
@@ -74,18 +73,17 @@ const data = [
 const Chart = () => {
   return (
     <div className="chart-container" width="100%" height="100%">
-      <LineChart width={600} height={400} data={data}>
+      <LineChart width={880} height={400} data={data}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" padding={{ left: 30, right: 30 }} />
         <YAxis />
         <Tooltip />
         <Line
           type="monotone"
-          dataKey="followersLast10days"
+          dataKey="newFollowers"
           stroke="hsl(243, 51%, 70%)"
           activeDot={{ r: 8 }}
         />
-        <Line type="monotone" dataKey="newFollowers" stroke="#82ca9d" />
       </LineChart>
     </div>
   );
